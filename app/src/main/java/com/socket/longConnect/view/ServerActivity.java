54f7927ad3b4bb4c0df0a85f0730d55e50057a6a
XunDiYang <1920310178@qq.com>
2023-06-25 17:57:49 +0800

    Update ServerActivity.java

--- app/src/main/java/com/socket/longConnect/view/ServerActivity.java
+++ app/src/main/java/com/socket/longConnect/view/ServerActivity.java
@@ -15,6 +15,20 @@ public class ServerActivity extends AppCompatActivity {
     private String ip = "127.0.0.1";
     private int port = 8888;
 
+    private NettyServerDemo.NettyServerBootStrap nettyServerBootStrap;
+    ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            nettyServerBootStrap = new NettyServerDemo.NettyServerBootStrap();
+            nettyServerBootStrap.connect();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +42,29 @@ public class ServerActivity extends AppCompatActivity {
         TextView txtlocalip = findViewById(R.id.localip);
         txtlocalip.setText(toString());
 
-         // new NettyServerDemo(port).start();
-
+        NettyServerDemo.setIp(ip);
+        NettyServerDemo.setPort(port);
+        NettyServerDemo.setHandler(msgHandler);
+        Intent intent = new Intent(this, NettyServerDemo.class);
+        bindService(intent,connection, Context.BIND_AUTO_CREATE);
     }
+
+    private Handler msgHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message message) {
+            int msgType = message.getData().getInt("type");
+            String clientIp = message.getData().getString("clientIp");
+            String rcvMsg = message.getData().getString("msg");
+            if (msgType == MsgType.CONNECTING){
+                Toast.makeText(ServerActivity.this,clientIp + " 连接成功",Toast.LENGTH_LONG).show();
+            }
+            else if (msgType == MsgType.TEXT && !TextUtils.isEmpty(rcvMsg)) {
+                TextView txtRcvMsg = findViewById(R.id.rcvMsg);
+                txtRcvMsg.setText(txtRcvMsg.getText().toString() + "\n\n收到消息来自于：" + clientIp + "\n消息内容："+ rcvMsg);
+            }
+            return false;
+        }
+    });
 
     @Override
     public String toString() {
