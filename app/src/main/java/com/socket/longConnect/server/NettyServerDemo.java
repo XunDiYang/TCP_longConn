f0e90ab163591843d27151ead7b1d9ba081b5ac3
XunDiYang <1920310178@qq.com>
2023-06-25 17:56:20 +0800

    Update NettyServerDemo.java

--- app/src/main/java/com/socket/longConnect/server/NettyServerDemo.java
+++ app/src/main/java/com/socket/longConnect/server/NettyServerDemo.java
@@ -10,34 +10,83 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 
-public class NettyServerDemo {
-    private int port = 8888;
+public class NettyServerDemo extends Service{
+    public static String ip = "127.0.0.1";
+    public static int port = 8888;
+    public static Handler handler;
+    public static ConnStatus status = ConnStatus.UNCONNED;
 
-    public NettyServerDemo(int port){
-        this.port = port;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
     }
 
-    public void run() {
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try{
-            ServerBootstrap  serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                protected void initChannel(SocketChannel ch) throws Exception {
-                    ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
-                    ch.pipeline().addLast(new StringDecoder());
-                    ch.pipeline().addLast(new ServerHandler());
-                }
-            });
-            ChannelFuture f = serverBootstrap.bind(port).sync();
-            System.out.println("ServerHandler started on " + port);
-            f.channel().closeFuture().sync();
-        }catch (Exception e){
-            e.getStackTrace();
-        }finally {
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
+    public static String getIp() {
+        return ip;
+    }
+
+    public static void setIp(String ip) {
+        NettyServerDemo.ip = ip;
+    }
+
+    public static int getPort() {
+        return port;
+    }
+
+    public static void setPort(int port) {
+        NettyServerDemo.port = port;
+    }
+
+    public static Handler getHandler() {
+        return handler;
+    }
+
+    public static void setHandler(Handler handler) {
+        NettyServerDemo.handler = handler;
+    }
+
+    private NettyServerBootStrap nettyServerBootStrap = new NettyServerBootStrap();
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return nettyServerBootStrap;
+    }
+
+    public static class NettyServerBootStrap extends Binder{
+        public void connect() {
+            EventLoopGroup bossGroup = new NioEventLoopGroup();
+            EventLoopGroup workerGroup = new NioEventLoopGroup();
+            try {
+                ServerBootstrap serverBootstrap = new ServerBootstrap();
+                serverBootstrap
+                        .group(bossGroup, workerGroup)
+                        .channel(NioServerSocketChannel.class)
+                        .childOption(ChannelOption.SO_KEEPALIVE,true)
+                        .childHandler(new ChannelInitializer<SocketChannel>() {
+                            @Override
+                            protected void initChannel(SocketChannel ch) throws Exception {
+                                ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                                ch.pipeline().addLast(new StringDecoder());
+                                ch.pipeline().addLast(new ServerHandler());
+                            }
+                        });
+                ChannelFuture f = serverBootstrap.bind(port).sync();
+                System.out.println("ServerHandler started on " + port);
+                f.channel().closeFuture().sync();
+            } catch (Exception e) {
+                e.getStackTrace();
+            } finally {
+                workerGroup.shutdownGracefully();
+                bossGroup.shutdownGracefully();
+            }
         }
     }
+
 }
