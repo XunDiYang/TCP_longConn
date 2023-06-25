cfd0368fe00a338667e7fc4403c0386090a66a0f
XunDiYang <1920310178@qq.com>
2023-06-25 17:58:32 +0800

    Create CMessage.java

---
+++ app/src/main/java/com/socket/longConnect/utils/CMessage.java
@@ -0,0 +1,60 @@
+public class CMessage implements Serializable {
+    @SerializedName("from")
+    private String from;
+    @SerializedName("to")
+    private String to;
+    @SerializedName("code")
+    private int code;
+    @SerializedName("type")
+    private int type;
+    @SerializedName("msg")
+    private String msg;
+
+
+    public String toJson() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+
+}
