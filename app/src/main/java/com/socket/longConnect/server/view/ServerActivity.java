9b6d3d72d56f1ca57a5061c6f945d6d4524e612b
XunDiYang <1920310178@qq.com>
2023-06-26 17:57:11 +0800

    Update ServerActivity.java

--- app/src/main/java/com/socket/longConnect/server/view/ServerActivity.java
+++ app/src/main/java/com/socket/longConnect/server/view/ServerActivity.java
@@ -24,50 +24,28 @@ public class ServerActivity extends AppCompatActivity {
     private String ip = "127.0.0.1";
     private int port = 8888;
 
-    private NettyServerDemo.NettyServerBootStrap nettyServerBootStrap;
-    ServiceConnection connection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-            nettyServerBootStrap = new NettyServerDemo.NettyServerBootStrap();
-            nettyServerBootStrap.connect();
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null) {
-            ip = bundle.getString("ip");
-            port = bundle.getInt("port");
-        }
 
+        NettyServerDemo serverService = NettyServerDemo.getServerService();
+        ip = serverService.getIp();
+        port = serverService.getPort();
         TextView txtlocalip = findViewById(R.id.localip);
         txtlocalip.setText(toString());
 
-        NettyServerDemo.setIp(ip);
-        NettyServerDemo.setPort(port);
-        NettyServerDemo.setHandler(msgHandler);
-        Intent intent = new Intent(this, NettyServerDemo.class);
-        bindService(intent,connection, Context.BIND_AUTO_CREATE);
     }
 
     private Handler msgHandler = new Handler(message -> {
         int msgType = message.getData().getInt("type");
         String clientIp = message.getData().getString("clientIp");
         String rcvMsg = message.getData().getString("msg");
-        if (msgType == MsgType.CONNECTING){
-            Toast.makeText(ServerActivity.this,clientIp + " 连接成功",Toast.LENGTH_LONG).show();
-        }
-        else if (msgType == MsgType.TEXT && !TextUtils.isEmpty(rcvMsg)) {
+        if (msgType == MsgType.CONNECT) {
+            Toast.makeText(ServerActivity.this, clientIp + " 连接成功", Toast.LENGTH_LONG).show();
+        } else if (msgType == MsgType.TEXT && !TextUtils.isEmpty(rcvMsg)) {
             TextView txtRcvMsg = findViewById(R.id.rcvMsg);
-            txtRcvMsg.setText(txtRcvMsg.getText().toString() + "\n\n收到消息来自于：" + clientIp + "\n消息内容："+ rcvMsg);
+            txtRcvMsg.setText(txtRcvMsg.getText().toString() + "\n\n收到消息来自于：" + clientIp + "\n消息内容：" + rcvMsg);
         }
         return false;
     });
