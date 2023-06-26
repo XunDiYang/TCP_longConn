a67bfb214b921e2133ea181977fc7b435b268ee5
XunDiYang <1920310178@qq.com>
2023-06-26 17:54:40 +0800

    Update ClientActivity.java

--- app/src/main/java/com/socket/longConnect/client/view/ClientActivity.java
+++ app/src/main/java/com/socket/longConnect/client/view/ClientActivity.java
@@ -2,29 +2,25 @@ package com.socket.longConnect.client.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.socket.longConnect.R;
 import com.socket.longConnect.client.service.NettyClientDemo;
-import com.socket.longConnect.utils.NetUtils;
-
-import java.net.SocketException;
 
 public class ClientActivity extends AppCompatActivity {
-
+    public TextView txtlocalip;
+    public TextView txtSndMsg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
 
-        TextView txtlocalip = findViewById(R.id.localip);
+        txtlocalip = findViewById(R.id.localip);
         txtlocalip.setText(ipPortToString());
 
-        TextView txtSndMsg = findViewById(R.id.sndMsg);
+        txtSndMsg = findViewById(R.id.sndMsg);
         String sndMsg = txtSndMsg.toString();
 
         Button btnSndMsg = findViewById(R.id.btnSndMsg);
@@ -34,10 +30,11 @@ public class ClientActivity extends AppCompatActivity {
 
     }
 
+
     public String ipPortToString() {
         return
                 "client ip='" + NettyClientDemo.getLocalIp() + '\'' +
-                        "server ip='" + NettyClientDemo.getServerIp() + '\'' +
+                        "\nserver ip='" + NettyClientDemo.getServerIp() + '\'' +
                         ", port=" + NettyClientDemo.getServerPort();
     }
 }
