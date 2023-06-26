17faa9f526d0826e612640f5810d713691aadba2
XunDiYang <1920310178@qq.com>
2023-06-26 17:55:46 +0800

    Update MsgType.java

--- app/src/main/java/com/socket/longConnect/model/MsgType.java
+++ app/src/main/java/com/socket/longConnect/model/MsgType.java
@@ -1,9 +1,9 @@
 package com.socket.longConnect.model;
 
 public interface MsgType {
-    int CONNECTING = 1;
+    int CONNECT = 1;
 
-    int CONNECT_SUCCESS = 2;
+    int TEXT = 2;
 
-    int TEXT = 3;
+    int PING = 3;
 }
