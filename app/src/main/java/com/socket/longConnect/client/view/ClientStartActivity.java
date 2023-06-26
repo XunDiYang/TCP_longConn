71c7bb8931afd30f664cc2221a28aeb6c7b272e1
XunDiYang <1920310178@qq.com>
2023-06-26 17:54:58 +0800

    Update ClientStartActivity.java

--- app/src/main/java/com/socket/longConnect/client/view/ClientStartActivity.java
+++ app/src/main/java/com/socket/longConnect/client/view/ClientStartActivity.java
@@ -4,22 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.socket.longConnect.R;
 import com.socket.longConnect.client.service.NettyClientDemo;
+import com.socket.longConnect.model.CMessage;
+import com.socket.longConnect.model.Callback;
 import com.socket.longConnect.utils.NetUtils;
 
 import java.net.SocketException;
 
 public class ClientStartActivity extends AppCompatActivity {
+    public TextView txtServerIp;
+    public TextView txtServerPort;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_start);
+
+        startService(new Intent(this, NettyClientDemo.class));
+        NettyClientDemo clientService = NettyClientDemo.getClientService();
 
         TextView txtlocalIp = findViewById(R.id.localip);
         String localip = "127.0.0.1";
@@ -30,20 +40,44 @@ public class ClientStartActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        TextView txtServerIp = findViewById(R.id.server_ip);
-        String serverIp = txtServerIp.toString();
-        TextView txtServerPort = findViewById(R.id.server_port);
-        int serverPort = Integer.parseInt(txtServerPort.toString());
-
         Button btnConnServer = findViewById(R.id.btnConnServer);
         String finalLocalip = localip;
         btnConnServer.setOnClickListener(v -> {
-            NettyClientDemo.setLocalIp(finalLocalip);
-            NettyClientDemo.setServerIp(serverIp);
-            NettyClientDemo.setServerPort(serverPort);
+            txtServerIp = findViewById(R.id.server_ip);
+            txtServerPort = findViewById(R.id.server_port);
 
-            Intent intent = new Intent(ClientStartActivity.this,ClientActivity.class);
-            startActivity(intent);
+            if (TextUtils.isEmpty(txtServerIp.getText()) || TextUtils.isEmpty(txtServerPort.getText())){
+                clientService.closeChannel();
+                Toast.makeText(ClientStartActivity.this, "请键入正确的Ip和端口号", Toast.LENGTH_LONG).show();
+            }else{
+                String serverIp = txtServerIp.getText().toString();
+                int serverPort = Integer.parseInt(txtServerPort.getText().toString());
+
+                clientService.setLocalIp(finalLocalip);
+                clientService.setServerIp(serverIp);
+                clientService.setServerPort(serverPort);
+
+                try {
+                    clientService.connect(connMsgCallback);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         });
     }
+
+    private Callback<CMessage> connMsgCallback = new Callback<CMessage>() {
+        @Override
+        public void onEvent(int code, String msg, CMessage cMessage) {
+            if (code == 200){
+                Toast.makeText(ClientStartActivity.this, "连接成功", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ClientStartActivity.this,ClientActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            else {
+                Toast.makeText(ClientStartActivity.this, "连接失败", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
